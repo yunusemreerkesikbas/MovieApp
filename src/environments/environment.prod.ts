2f2c1f9600679072635b93c45c34bf4b3963747c
yunusemreerkesikbas <yunusemreyee64@gmail.com>
2023-07-09 01:16:59 +0300

    updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  ROOT_URL: 'https://api.themoviedb.org/3/',
+  MOVIEDB_API_KEY: '9e6ffae09b5fc437cc13f827f2882fe9',
+  MOVIEDB_API_ACCESS_TOKEN:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTZmZmFlMDliNWZjNDM3Y2MxM2Y4MjdmMjg4MmZlOSIsInN1YiI6IjY0YTY4MGRhMDdmYWEyMDEzYjQxMWMzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Wc2kaUoi15Con4PzRGM7kz_5HKS8EgHmc12XPA4lgjI',
 };
